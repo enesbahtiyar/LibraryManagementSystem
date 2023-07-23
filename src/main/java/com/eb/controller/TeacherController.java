9358e77d1b7a1e45fd467d2200bb9bb6923f32fe
Enes Bahtiyar <97892255+enesbahtiyar@users.noreply.github.com>
2023-07-23 21:48:57 +0300

    endpoints

--- src/main/java/com/eb/controller/TeacherController.java
+++ src/main/java/com/eb/controller/TeacherController.java
@@ -1,12 +1,61 @@
 package com.eb.controller;
 
+import com.eb.domain.Teacher;
 import com.eb.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.PushBuilder;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/teachers") //http://localhost:8080/teachers
 public class TeacherController
 {
     @Autowired
     private TeacherService teacherService;
+
+    @PostMapping //http://localhost:8080/teachers
+    public ResponseEntity<Map<String, String>> saveTeacher(@Valid @RequestBody Teacher teacher)
+    {
+        teacherService.saveTeacher(teacher);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("message", "teacher is successfully saved");
+        map.put("success", "true");
+
+        return new ResponseEntity<>(map, HttpStatus.CREATED); //201
+    }
+
+    @GetMapping //http://localhost:8080/teachers
+    public ResponseEntity<List<Teacher>> getAllTeachers()
+    {
+        List<Teacher> teachers = teacherService.getAllTeachers();
+
+        return ResponseEntity.ok(teachers);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Teacher> findTeacherById(@PathVariable Long id)
+    {
+        Teacher teacher = teacherService.findTeacherById(id);
+
+        return ResponseEntity.ok(teacher);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteTeacherById(@PathVariable Long id)
+    {
+
+        teacherService.deleteTeacherById(id);
+
+        return new ResponseEntity<>("deleted teachers id: " + id, HttpStatus.OK);
+    }
 }
--- src/main/java/com/eb/service/TeacherService.java
+++ src/main/java/com/eb/service/TeacherService.java
@@ -1,13 +1,54 @@
 package com.eb.service;
 
+import com.eb.domain.Teacher;
+import com.eb.exception.ConflictException;
+import com.eb.exception.ResourceNotFoundException;
 import com.eb.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TeacherService
 {
     @Autowired
     private TeacherRepository repository;
+
+    public Teacher saveTeacher(Teacher teacher)
+    {
+        Teacher existTeacher = repository.findByEmail(teacher.getEmail());
+
+        if(existTeacher != null)
+        {
+            throw new ConflictException("Teacher with the same email already exists");
+        }
+        return repository.save(teacher);
+    }
+
+    public List<Teacher> getAllTeachers()
+    {
+        List<Teacher> teachers = repository.findAll();
+
+        if(teachers.isEmpty())
+        {
+            throw new ResourceNotFoundException("teacher list is empty");
+        }
+
+        return teachers;
+    }
+
+    public Teacher findTeacherById(Long id)
+    {
+        return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("the teacher couldn't find with the given id"));
+    }
+
+    public void deleteTeacherById(Long id)
+    {
+        Teacher teacher = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("this id does not exist"));
+
+        repository.delete(teacher);
+    }
 }
